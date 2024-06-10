487e8ad0eb0d253fb3a6c1bec5feffc819f1eccb
krbp <165799825+krbp@users.noreply.github.com>
2024-06-10 13:40:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp4
+{
+    public enum ECivilities
+    {
+        MONSIEUR,
+        MADAME
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new("A", "A", Couleur.BLEU);
+            user.Civilitie = ECivilities.MONSIEUR;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class User
+    {
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public Couleur CouleurPreferee { get; set; }
+
+        public User(string prenom, string nom, Couleur couleurPreferee)
+        {
+            Prenom = prenom;
+            Nom = nom;
+            CouleurPreferee = couleurPreferee;
+        }
+
+        public ECivilities Civilitie { get; set; }
+    }
+}
